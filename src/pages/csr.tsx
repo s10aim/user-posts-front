fe12344392ce04ec758f6873085fd9b8c7e0ed35
tanahara <s10.vermouth.4869@gmail.com>
2021-08-20 22:52:43 +0900

    CSRの一覧表示機能を実装

--- src/pages/csr.tsx
+++ src/pages/csr.tsx
@@ -1,10 +1,37 @@
 import type { NextPage } from 'next'
+import { useState, useEffect } from 'react'
+import { format } from 'date-fns'
 import { MetaHead } from '../components/MetaHead'
 import { Links } from '../components/Links'
 import styles from '../styles/Home.module.css'
+import { globalAxios, errorHandring } from '../service/globalAxios'
+
+type PostType = {
+  id: number
+  username: string
+  body: string
+  createdAt: string
+  updatedAt: string
+}
 
 const Csr: NextPage = () => {
   const title = '投稿一覧(CSR)'
+
+  const [posts, setPosts] = useState<PostType[]>([])
+  const fetchPosts = async () => {
+    try {
+      const response = await globalAxios.get('/posts')
+      const posts = response.data
+      setPosts(posts)
+    } catch (error) {
+      errorHandring(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
   return (
     <div className={styles.container}>
       <MetaHead title={title} />
@@ -12,6 +39,15 @@ const Csr: NextPage = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>{title}</h1>
         <Links />
+        {posts.map((post) => (
+          <div key={post.id}>
+            <div>投稿者名: {post.username}</div>
+            <div>内容: {post.body}</div>
+            <div>投稿日時: {format(new Date(post.createdAt), 'yyyy/MM/dd HH:mm:ss')}</div>
+            <div>更新日時: {format(new Date(post.updatedAt), 'yyyy/MM/dd HH:mm:ss')}</div>
+            <hr></hr>
+          </div>
+        ))}
       </main>
     </div>
   )
---
+++ src/service/globalAxios.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import type { AxiosError } from 'axios'
+
+const globalAxios = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
+  headers: {
+    'Content-type': 'apprication/json',
+  },
+  responseType: 'json',
+})
+
+const errorHandring = (error: AxiosError) => {
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    console.log(error.response.data)
+    console.log(error.response.status)
+    console.log(error.response.headers)
+  } else if (error.request) {
+    // The request was made but no response was received
+    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    // http.ClientRequest in node.js
+    console.log(error.request)
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.log('Error', error.message)
+  }
+  console.log(error.config)
+}
+
+export { globalAxios, errorHandring }
