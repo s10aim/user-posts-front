dd3c4a47e6d02a0066c0bb743ef0ec9a6cbbbab8
tanahara <s10.vermouth.4869@gmail.com>
2021-08-25 21:56:35 +0900

    SSRの一覧表示機能を実装

---
+++ src/pages/ssr.tsx
@@ -0,0 +1,39 @@
+import type { NextPage, GetServerSideProps } from 'next'
+import { MetaHead } from '../components/MetaHead'
+import { Post } from '../components/Post'
+import { fetchPostsService } from '../service/posts'
+import type { PostType } from '../types/post'
+
+type Props = {
+  posts: PostType[]
+}
+
+const Ssr: NextPage<Props> = (props) => {
+  const title = '投稿一覧(SSR)'
+  const { posts } = props
+  return (
+    <div>
+      <MetaHead title={title} />
+
+      <main>
+        <h1>{title}</h1>
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </main>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  try {
+    const posts = await fetchPostsService()
+    return {
+      props: { posts },
+    }
+  } catch (e) {
+    return { notFound: true }
+  }
+}
+
+export default Ssr
