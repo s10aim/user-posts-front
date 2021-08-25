57426ea7049c0e89a82d81d171b587934ecae9b9
tanahara <s10.vermouth.4869@gmail.com>
2021-08-25 21:31:43 +0900

    SSGの一覧表示機能を実装

--- src/pages/ssg.tsx
+++ src/pages/ssg.tsx
@@ -1,17 +1,39 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetStaticProps } from 'next'
 import { MetaHead } from '../components/MetaHead'
+import { Post } from '../components/Post'
+import { fetchPostsService } from '../service/posts'
+import type { PostType } from '../types/post'
 
-const Ssg: NextPage = () => {
+type Props = {
+  posts: PostType[]
+}
+
+const Ssg: NextPage<Props> = (props) => {
   const title = '投稿一覧(SSG)'
+  const { posts } = props
   return (
     <div>
       <MetaHead title={title} />
 
       <main>
         <h1>{title}</h1>
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
       </main>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const posts = await fetchPostsService()
+    return {
+      props: { posts },
+    }
+  } catch (e) {
+    return { notFound: true }
+  }
 }
 
 export default Ssg
