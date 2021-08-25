e3774787676e4a4e476cca7047cb97ab552ea30c
tanahara <s10.vermouth.4869@gmail.com>
2021-08-25 21:43:45 +0900

    CSR一覧表示のパフォーマンス向上

--- src/pages/csr.tsx
+++ src/pages/csr.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { format } from 'date-fns'
 
 import { MetaHead } from '../components/MetaHead'
@@ -14,18 +14,18 @@ const Csr: NextPage = () => {
   const title = '投稿一覧(CSR)'
 
   const [posts, setPosts] = useState<PostType[]>([])
-  const fetchPosts = async () => {
+  const fetchPosts = useCallback(async () => {
     try {
       const posts = await fetchPostsService()
       setPosts(posts)
     } catch (error) {
       errorHandring(error)
     }
-  }
+  }, [])
 
   useEffect(() => {
     fetchPosts()
-  }, [])
+  }, [fetchPosts])
 
   return (
     <div>
